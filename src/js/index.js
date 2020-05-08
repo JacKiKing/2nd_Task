d9bf2803897e551830751b11f3d856e9afacdd0a
Alexandr Chernetsov <yankee235150@gmail.com>
2020-05-08 14:53:32 +0200

    Reinstall webpack. Complead in pixel perfect 'Color and type'

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import jQuery from "jquery";
+import popper from "popper.js";
+import bootstrap from "bootstrap";
+import "./script.js";
+import "../scss/style.scss";
+
+import $ from 'jquery';
+ 
+global.jQuery = $;
+global.$ = $;
